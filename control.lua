37cb8e02fc995701bb8bb1264b1a598fe1d1ce9b
SirHall <sirhallstein@gmail.com>
2021-08-11 17:54:35 +1000

    Refactor to hopefully make multiplayer compatible

--- control.lua
+++ control.lua
@@ -1,5 +1,3 @@
-player_time_text = player_time_text or nil
-
 -- Because of my supreme laziness this is a modified version of: https://stackoverflow.com/a/45376848
 function FormatTime(time)
     local days = math.floor(time/86400)
@@ -38,46 +36,47 @@ function GetCraftTime(player)
     return craftTime;
 end
 
-function SetupText()
-    if player_time_text ~= nil then
-        return
+function SetupText(player)
+    if (not player) or (not player.character) then
+        return false
     end
-    
-    local player = game.get_player(1)
-    local col = {r = 1.0, g = 1.0, b = 1.0, a = 1.0}
-
-    player_time_text = rendering.draw_text({text = "", surface = "nauvis", target = player.character, color = col})
+    if global.player_time_text[player.index] == nil then
+        global.player_time_text[player.index] = rendering.draw_text({text = "", surface = "nauvis", target = player.character, color = {r = 1.0, g = 1.0, b = 1.0, a = 1.0}})
+    end
+    return true
 end
 
 function PrintCraftTime()
+    global.player_time_text = global.player_time_text or {}
 
-    if player_time_text == nil then
-        SetupText()
-    end
-    
     -- Go back to using the for loop to make this multiplayer compatible
-    -- for i, player in ipairs(game.players) do
-    local player = game.get_player(1)
-    if (not player) or (not player.character) then
-        rendering.set_visible(player_time_text, false)
-        return
+    for i, player in ipairs(game.connected_players) do
+
+        if player and player.character then
+            -- If the player's text has not been setup yet (somehow), ensure that we do
+            if global.player_time_text[player.index] == nil then
+                SetupText(player)
+            end
+
+            -- It is somehow possible for a player's time text to not exist, it may seem
+            if global.player_time_text[player.index] then
+                local time = GetCraftTime(player)
+
+                -- Only update visibility if it has changed
+                if (time > 0.1) ~= rendering.get_visible(global.player_time_text[player.index]) then
+                    rendering.set_visible(global.player_time_text[player.index], time > 0.1)
+                end
+
+                -- rendering.set_text(player_time_text, "" .. string.format("%.1f", time) .. "s")
+                rendering.set_text(global.player_time_text[player.index], FormatTime(time))
+            end
+        end
     end
-
-    local time = GetCraftTime(player)
-
-    -- Only update visibility if it has changed
-    if (time > 0.1) ~= rendering.get_visible(player_time_text) then
-        rendering.set_visible(player_time_text, time > 0.1)
-    end
-
-    -- rendering.set_text(player_time_text, "" .. string.format("%.1f", time) .. "s")
-    rendering.set_text(player_time_text, FormatTime(time))
-    -- end
 
     -- game.players[1].print("Time: " .. GetCraftTime(e.player_index) .. "s")
 end
 
+script.on_load(OnLoad)
 script.on_event(defines.events.on_tick, PrintCraftTime)
--- script.on_event(defines.events.on_pre_player_crafted_item, PrintCraftTime)
--- script.on_event(defines.events.on_player_crafted_item, PrintCraftTime)
--- script.on_event(defines.events.on_player_cancelled_crafting, PrintCraftTime)
+-- script.on_event(defines.events.on_player_joined_game, PlayerJoined)
+-- script.on_event(defines.events.on_player_left_game, PlayerLeft)
